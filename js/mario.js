74d51cd182dbb89167e34a265669ae9584c73773
Syamsul <syamsul@alterra.id>
2019-08-07 15:43:23 +0700

    detect collision

--- js/mario.js
+++ js/mario.js
@@ -1,33 +1,50 @@
-function navigate(event){
-    let button = event.keyCode
-    let mario = document.getElementById('mario')
+let mario = document.getElementById('mario')
+let marioX = parseInt(getComputedStyle(mario).left, 10)
+let marioY = parseInt(getComputedStyle(mario).bottom, 10)
 
-    mario.style.left = 100+'px'
-    mario.style.top = -100+'px'
+// function navigate(event){
+//     let button = event.keyCode
+//     let mario = document.getElementById(mario)
 
-    
+//     mario.style.left = 100+'px'
+//     mario.style.top = -100+'px'
+// }
+
+function isCollide(a, b){
+    if ( (a > 160 && a < 240) && b>60){
+        return true
+    }
+
+    if (a>460){
+        return true
+    }
+
+    return false
 }
 
 function jump(event){
     var space = event.keyCode
-
-    let mario = document.getElementById('mario')
-    let marioCss = getComputedStyle(mario)
-
-    let curH = parseInt(marioCss.top, 10)
     let dist = 0
-
+    let j
+    console.log('X',marioX)
+    console.log('Y',marioY)
     if (space == 32){
         let j = setInterval(toJump, 0)
     }
-
+    
+    
     function toJump(){
+        if (isCollide(marioX, marioY)){
+            alert('game over')
+            location.reload()
+        }
+        
         if (dist < 100){
-            curH--
-            mario.style.top = curH + 'px'
+            marioY++
+            mario.style.bottom = marioY + 'px'
         } else if (dist < 200) {
-            curH++
-            mario.style.top = curH + 'px'
+            marioY--
+            mario.style.bottom = marioY + 'px'
         } else if (dist == 200) {
             clearInterval(j)
         }
@@ -39,22 +56,35 @@ function jump(event){
 
 function move(event) {
     var k = event.keyCode
-    chrId = document.getElementById('mario')
+   
     chr = {
         leftright: function () {
-            var x = parseInt(getComputedStyle(chrId).left);
-            if (k == 65 || k == 33) {
-                x-=10;
-            } else if (k == 68 || k == 34) {
-                x+=10;
+            if (isCollide(marioX, marioY)){
+                alert('game over')
+                location.reload()
             }
+
+            if (k == 65 || k == 33) {
+                marioX-=10;
+            } else if (k == 68 || k == 34) {
+                marioX+=10;
+            } else if ( k == 87) {
+                marioY+=5;
+            } else if (k == 83) {
+                marioY-=5;
+            }
+
+            console.log('X', marioX)
+            console.log('Y',marioY)            
             
-            return x;
+            return [marioX, marioY];
         }
     };
 
-    // chrId.style.top = (chr.updown()) + "px";
-    chrId.style.left = (chr.leftright()) + "px";
+    // mario.style.top = (chr.updown()) + "px";
+    mario.style.left = (chr.leftright()[0]) + "px";
+    mario.style.bottom = (chr.leftright()[1]) + "px";
+
 }
 
 document.addEventListener('keydown', jump);
