4ba5dc65e9b6dea6a733704120b73d0f6a990d54
Ahmad Rizqita <rizqita@alterra.id>
2019-08-07 13:42:00 +0700

    left right control free

--- js/mario.js
+++ js/mario.js
@@ -1,6 +1,21 @@
-function move(){
-    let mario = document.getElementById('mario')
-    console.log(mario)
-    mario.style.left = 100+'px'
-    mario.style.top = -100+'px'
+  function move(event) {
+    var k = event.keyCode,
+        chrId = document.getElementById('mario'),
+        chr = {
+            leftright: function () {
+                var x = parseInt(getComputedStyle(chrId).left);
+                if (k == 65) {
+                   x-=10;
+                } else if (k == 68) {
+                    x+=10;
+                }
+                
+                return x;
+            }
+        };
+
+    // chrId.style.top = (chr.updown()) + "px";
+    chrId.style.left = (chr.leftright()) + "px";
 }
+
+document.addEventListener('keydown', move);
